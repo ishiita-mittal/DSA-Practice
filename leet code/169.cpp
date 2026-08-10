5a51d5e16504a4b44261249b6968ea6b5c02692c
ishiita-mittal <ishiita.mittal15@gmail.com>
2026-08-10 14:42:47 +0530

    Practiced more leet code questions

---
+++ leet code/169.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+    int majorityElement(vector<int>& nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == candidate)
+                count++;
+            else
+                count--;
+
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            }
+        }
+
+        return candidate;
+    }
+
+    int main() {
+        vector<int>arr={3,2,3};
+        cout<<majorityElement(arr);
+        return 0;
+    }
---
+++ leet code/704.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+    int search(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1;
+        while(start<=end) {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            else if(nums[mid]<target) {
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+int main() {
+           vector<int>nums={2,4,6,8,10,12};
+           int target=10;
+           cout<<search(nums,target); 
+    return 0;
+}
