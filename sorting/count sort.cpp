f12b1366b563694c6a492f0442b0a3a385a06cf8
ishiita-mittal <ishiita.mittal15@gmail.com>
2026-08-14 21:00:00 +0530

    Added complexity analysis

---
+++ sorting/count sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void countSort(int *arr, int size) {
+    int minVal=INT32_MAX, maxVal=INT32_MIN;
+    int freq[100000];
+    for(int i=0; i<size; i++) {
+        minVal=min(minVal, arr[i]);
+        maxVal=max(maxVal, arr[i]);
+    }
+    for(int i=0; i<size; i++) {
+        freq[arr[i]]++;
+    }
+    for(int i=minVal, j=0; i<=maxVal; i++) {
+        while(freq[i]>0) {
+            arr[j++]=i;
+            freq[i]--;
+        }
+    }
+    for(int i=0; i<size; i++) {
+          cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+                    
+int main() {
+           int arr[]={1,4,1,3,2,4,3,7};
+           int size=sizeof(arr)/sizeof(int);
+           countSort(arr, size); 
+    return 0;
+}
