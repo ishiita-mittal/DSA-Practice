b30953ba6b057771eae69f6dca492f6f40457aea
ishiita-mittal <ishiita.mittal15@gmail.com>
2026-08-31 22:08:02 +0530

    learning

---
+++ 2D Arrays/spiral matrix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void spiralMatrix(int matrix[][4], int row, int col) {
+
+}
+                    
+int main() {
+    int matrix[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    sizeof(matrix)/sizeof(matrix[0]);
+    sizeof(matrix[0])/sizeof(matrix[0][0]);        
+    return 0;
+}
